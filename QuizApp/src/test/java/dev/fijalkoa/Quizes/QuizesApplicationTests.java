891d8ed09687703826b7656936dded9533353edb
fijalkoa <01178834@pw.edu.pl>
2024-06-01 22:50:24 +0200

    Adding project files to the repository

---
+++ QuizApp/src/test/java/dev/fijalkoa/Quizes/QuizesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.fijalkoa.Quizes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
